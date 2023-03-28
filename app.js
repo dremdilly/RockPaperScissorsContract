4ed59fb5698fafcb487d1aa8996401a48ed7b542
Daurenbek Beknazarov <drenbek@gmail.com>
2023-03-28 16:42:06 +0600

    fix

--- app.js
+++ app.js
@@ -313,7 +313,7 @@ async function displayGameHistory() {
 }
   
 function getResult(playerMove, houseMove) {
-    const moveCombination = playerMove.toLowerCase.charAt[0] + houseMove.toLowerCase.charAt[0];
+    const moveCombination = playerMove.toLowerCase().charAt(0) + houseMove.toLowerCase().charAt(0);
 
     switch (moveCombination) {
         case "pr":
